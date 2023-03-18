1101652ee817cb298ac13b222da684e1279ee550
Lucreciaaaa <113994468+Lucreciaaaa@users.noreply.github.com>
2023-03-18 12:34:59 +0100

    suggestions

--- app/src/main/java/com/example/unify/InterfacePrincipale.java
+++ app/src/main/java/com/example/unify/InterfacePrincipale.java
@@ -9,5 +9,6 @@ public class InterfacePrincipale extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interface_principale);
+        setContentView(R.layout.menu_suggestions) ;
     }
 }
