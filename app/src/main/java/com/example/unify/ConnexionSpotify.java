dec4843d88d6fa88dfe816975a677a4588f708ac
Lucreciaaaa <113994468+Lucreciaaaa@users.noreply.github.com>
2023-03-20 16:01:10 +0100

    recherche musique

--- app/src/main/java/com/example/unify/ConnexionSpotify.java
+++ app/src/main/java/com/example/unify/ConnexionSpotify.java
@@ -8,6 +8,6 @@ public class ConnexionSpotify extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connexion_spotify);
+        setContentView(R.layout.activity_connexion_spotify_opt);
     }
 }
