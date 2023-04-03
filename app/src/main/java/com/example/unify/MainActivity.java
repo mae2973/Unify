d0b44ca049f4a2b74b5121da24f9f906a27fd25d
CATS <11648807+CoAuToSe@users.noreply.github.com>
2023-04-03 16:42:23 +0200

    correction erreurs

--- app/src/main/java/com/example/unify/MainActivity.java
+++ app/src/main/java/com/example/unify/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
+        // fab = floating_action_button #AndroidStudioIntelo
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
