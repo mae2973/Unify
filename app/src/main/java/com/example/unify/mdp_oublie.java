bb67515a2b3d22e3c9dff6408e0ce265b83b24ae
NovaSkyrim <100930779+NovaSkyrim@users.noreply.github.com>
2023-04-03 14:32:52 +0200

    Clem

--- app/src/main/java/com/example/unify/mdp_oublie.java
+++ app/src/main/java/com/example/unify/mdp_oublie.java
@@ -2,21 +2,23 @@ package com.example.unify;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class mdp_oublie extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mdp_oublie);
+        setContentView(R.layout.overlay_mdp_oublie);
     }
 
-    Button button_validation = (Button)findViewById(R.id.open_activity_button);
+    Button button_validation = (Button)findViewById(R.id.button_validation);
 
-    btn.setOnClickListener(new View.OnClickListener() {
+    Button.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             startActivity(new Intent(MainActivity.this, MyOtherActivity.class));
         }
     });
-}
+}d
