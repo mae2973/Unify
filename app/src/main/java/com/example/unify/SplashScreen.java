50c2c5f255b2c6873e4bf9839354e1bad6ca66da
Lucreciaaaa <113994468+Lucreciaaaa@users.noreply.github.com>
2023-04-23 16:38:59 +0200

    splash screen

--- app/src/main/java/com/example/unify/SplashScreen.java
+++ app/src/main/java/com/example/unify/SplashScreen.java
@@ -1,14 +1,36 @@
 package com.example.unify;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os. Handler ;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private final int SPLASH_SCREEN_TIMEOUT = 3000 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+
+        // Rediriger vers la page Connexion après 3 secondes
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                // Démarrer une page
+                Intent intent = new Intent(getApplicationContext(),page_connexion.class) ;
+                startActivity(intent) ;
+                finish();
+            }
+        } ;
+
+
+
+        // Création d'un Handler post delayed
+        new Handler().postDelayed(runnable,SPLASH_SCREEN_TIMEOUT) ;
+
+
     }
 }
