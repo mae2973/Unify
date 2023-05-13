f2b89590e4c8d14b4d86f9d6a4ba24646a492faa
mae2973 <114489922+mae2973@users.noreply.github.com>
2023-05-13 09:03:14 +0200

    update

--- app/src/main/java/com/example/unify/D_CreationCompte.java
+++ app/src/main/java/com/example/unify/D_CreationCompte.java
@@ -7,14 +7,18 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,20 +43,20 @@ public class D_CreationCompte extends AppCompatActivity {
             }
         });
 
-        // BOUTON POUR VALIDER ( s'active seulement si tous les champs sont remplis )
+        // BOUTON POUR VALIDER (s'active seulement si tous les champs sont remplis)
         EditText nom2 = findViewById(R.id.nom_user);
-        EditText prenom2= findViewById(R.id.prenom_user);
+        EditText prenom2 = findViewById(R.id.prenom_user);
         EditText mail2 = findViewById(R.id.mail_user);
         EditText identifiant2 = findViewById(R.id.identifiant_user);
         EditText mdp2 = findViewById(R.id.mdp_user);
         EditText confirm_mdp2 = findViewById(R.id.confirm_mdp_user);
-
 
         buttonValider.setEnabled(false);
 
         TextWatcher formulaireValidation = new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 String nom = nom2.getText().toString().trim();
@@ -67,62 +71,99 @@ public class D_CreationCompte extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {}
         };
+
         buttonValider.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseFirestore db = FirebaseFirestore.getInstance();
-                //String documentName = "user5";
 
-                EditText nom = findViewById(R.id.nom_user);
-                String nom_text = nom.getText().toString();
-
-                EditText prenom= findViewById(R.id.prenom_user);
-                String prenom_text = prenom.getText().toString();
-
-                EditText mail = findViewById(R.id.mail_user);
-                String mail_text = mail.getText().toString();
-
-                EditText identifiant  = findViewById(R.id.identifiant_user);
+                // Récupérer l'identifiant saisi
+                EditText identifiant = findViewById(R.id.identifiant_user);
                 String id_text = identifiant.getText().toString();
 
-                EditText mdp = findViewById(R.id.mdp_user);
-                String mdp_text = mdp.getText().toString();
-
-               // EditText conf_mdp = findViewById(R.id.confirm_mdp_user);
-                //String conf_mdp_text = mdp.getText().toString();
-                // Accédez à la collection et créez le document avec le nom choisi
-                String documentName = id_text;
-                DocumentReference documentRef = db.collection("user").document(documentName);
-
-                // Créez les données à ajouter au document
-                Map<String, Object> data = new HashMap<>();
-                data.put("nom", nom_text);
-                data.put("prenom", prenom_text);
-                data.put("mail", mail_text);
-                // TODO verif que mdp = confirm mdp
-                data.put("identifiant", id_text);
-                data.put("mdp", mdp_text);
-
-// Ajoutez le document à la base de données
-                documentRef.set(data)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                // Vérifier si l'identifiant existe déjà
+                db.collection("user")
+                        .whereEqualTo("identifiant", id_text)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                             @Override
-                            public void onSuccess(Void aVoid) {
-                                // Document créé avec succès
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                // Erreur lors de la création du document
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    QuerySnapshot querySnapshot = task.getResult();
+                                    if (querySnapshot != null && !querySnapshot.isEmpty()) {
+                                        // L'identifiant existe déjà, afficher un message d'erreur ou prendre une action appropriée
+                                        Toast.makeText(D_CreationCompte.this, "Cet identifiant existe déjà.", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        // L'identifiant n'existe pas, ajouter le document à la base de données
+
+                                        // Génération du nom du document
+                                        db.collection("user")
+                                                .get()
+                                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                        if (task.isSuccessful()) {
+                                                            int count = task.getResult().size();
+                                                            String documentName = "user" + (count + 1);
+
+                                                            // Accédez à la collection et créez le document avec le nom choisi
+                                                            DocumentReference documentRef = db.collection("user").document(documentName);
+
+                                                            EditText nom = findViewById(R.id.nom_user);
+                                                            String nom_text = nom.getText().toString();
+
+                                                            EditText prenom = findViewById(R.id.prenom_user);
+                                                            String prenom_text = prenom.getText().toString();
+
+                                                            EditText mail = findViewById(R.id.mail_user);
+                                                            String mail_text = mail.getText().toString();
+
+                                                            EditText mdp = findViewById(R.id.mdp_user);
+                                                            String mdp_text = mdp.getText().toString();
+
+                                                            // Créez les données à ajouter au document
+                                                            Map<String, Object> data = new HashMap<>();
+                                                            data.put("nom", nom_text);
+                                                            data.put("prenom", prenom_text);
+                                                            data.put("mail", mail_text);
+                                                            // TODO: Vérifier que mdp = confirm mdp
+                                                            data.put("identifiant", id_text);
+                                                            data.put("mdp", mdp_text);
+
+                                                            // Ajoutez le document à la base de données
+                                                            documentRef.set(data)
+                                                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                        @Override
+                                                                        public void onSuccess(Void aVoid) {
+                                                                            // Document créé avec succès
+                                                                        }
+                                                                    })
+                                                                    .addOnFailureListener(new OnFailureListener() {
+                                                                        @Override
+                                                                        public void onFailure(@NonNull Exception e) {
+                                                                            // Erreur lors de la création du document
+                                                                        }
+                                                                    });
+
+                                                            Intent switchActivityIntent = new Intent(D_CreationCompte.this, B_PageConnexion.class);
+                                                            startActivity(switchActivityIntent);
+                                                            overridePendingTransition(0, 0);
+                                                        } else {
+                                                            // Une erreur s'est produite lors de la récupération du nombre de documents
+                                                            Toast.makeText(D_CreationCompte.this, "Erreur lors de la génération du nom du document.", Toast.LENGTH_SHORT).show();
+                                                        }
+                                                    }
+                                                });
+                                    }
+                                } else {
+                                    // Une erreur s'est produite lors de la vérification
+                                    Toast.makeText(D_CreationCompte.this, "Erreur lors de la vérification de l'identifiant.", Toast.LENGTH_SHORT).show();
+                                }
                             }
                         });
-
-                Intent switchActivityIntent = new Intent(D_CreationCompte.this, B_PageConnexion.class);
-                startActivity(switchActivityIntent);
-                overridePendingTransition(0, 0);
             }
         });
+
         prenom2.addTextChangedListener(formulaireValidation);
         nom2.addTextChangedListener(formulaireValidation);
         mail2.addTextChangedListener(formulaireValidation);
@@ -131,11 +172,11 @@ public class D_CreationCompte extends AppCompatActivity {
         confirm_mdp2.addTextChangedListener(formulaireValidation);
     }
 
-
-
     private void setButtonAnnuler() {
         Intent switchActivityIntent = new Intent(this, B_PageConnexion.class);
         startActivity(switchActivityIntent);
         overridePendingTransition(0, 0);
     }
 }
+
+
