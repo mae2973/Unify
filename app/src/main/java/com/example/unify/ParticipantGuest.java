0a0d30833c685ec0c5b6ebf75038f0f4970bfb84
Lucreciaaaa <113994468+Lucreciaaaa@users.noreply.github.com>
2023-05-14 14:30:18 +0200

    boutons interface p guest

---
+++ app/src/main/java/com/example/unify/ParticipantGuest.java
@@ -0,0 +1,14 @@
+package com.example.unify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParticipantGuest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_participant_guest);
+    }
+}
