9cbfcdec88503ef0e86b9d3314734e75023e4188
NovaSkyrim <100930779+NovaSkyrim@users.noreply.github.com>
2023-05-14 16:54:57 +0200

    SpotifyPlayerok

--- app/src/main/java/com/example/unify/SpotifyPlayer.java
+++ app/src/main/java/com/example/unify/SpotifyPlayer.java
@@ -4,70 +4,58 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
-import com.spotify.android.appremote.api.error.AuthenticationFailedException;
-import com.spotify.android.appremote.api.error.CouldNotFindSpotifyApp;
-import com.spotify.android.appremote.api.error.NotLoggedInException;
 import com.spotify.android.appremote.api.error.SpotifyConnectionTerminatedException;
+import com.spotify.android.appremote.api.error.SpotifyRemoteServiceException;
 import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
+public class SpotifyPlayer extends AppCompatActivity {
 
-public class SpotifyPlayer {
-
-    private static final String CLIENT_ID = "your_client_id";
-    private static final String REDIRECT_URI = "your_redirect_uri";
-    private static final String TAG = "SpotifyPlayer";
-    private static final String SPOTIFY_PACKAGE_NAME = "com.spotify.music";
+    private static final String CLIENT_ID = "9ef8387d89894e118397248505847c47";
+    private static final String REDIRECT_URI = "my-app://callback";
+    private static final String ACCESS_TOKEN = "your_access_token_here";
 
     private SpotifyAppRemote mSpotifyAppRemote;
 
-    public void connectToSpotify(Context context, String accessToken) {
-        ConnectionParams connectionParams = new ConnectionParams.Builder(CLIENT_ID)
-                .setRedirectUri(REDIRECT_URI)
-                .setAccessToken(accessToken)
-                .build();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        SpotifyAppRemote.connect(context, connectionParams, new SpotifyAppRemote.Connector.ConnectionListener() {
-            @Override
-            public void onConnected(SpotifyAppRemote spotifyAppRemote) {
-                mSpotifyAppRemote = spotifyAppRemote;
-                Log.d(TAG, "Connected to Spotify");
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .build();
 
-                // Start playing a playlist
-                mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DWZeAduKwuwrr");
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
 
-                // Subscribe to player state changes
-                Subscription<PlayerState> playerStateSubscription =
-                        mSpotifyAppRemote.getPlayerApi().subscribeToPlayerState().setEventCallback(playerState -> {
-                            final Track track = playerState.track;
-                            if (track != null) {
-                                Log.d(TAG, track.name + " by " + track.artist.name);
-                            }
-                        });
-            }
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
 
-            @Override
-            public void onFailure(Throwable throwable) {
-                Log.e(TAG, "Error connecting to Spotify: " + throwable.getMessage());
-                if (throwable instanceof AuthenticationFailedException) {
-                    // Authentication failed!
-                } else if (throwable instanceof CouldNotFindSpotifyApp) {
-                    // Spotify app not installed or not launched
-                } else if (throwable instanceof Exception) {
-                    // User logged out of Spotify
-                } else if (throwable instanceof NotLoggedInException) {
-                    // User not logged in to Spotify
-                } else if (throwable instanceof SpotifyConnectionTerminatedException) {
-                    // Connection to Spotify app was terminated
-                }
-            }
-        });
+                        // Play a playlist
+                        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DWZeAduKwuwrr");
+                    }
+
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MainActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
     }
 
-    public void disconnectFromSpotify() {
+    @Override
+    protected void onStop() {
+        super.onStop();
         SpotifyAppRemote.disconnect(mSpotifyAppRemote);
     }
-
 }
